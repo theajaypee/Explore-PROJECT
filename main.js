4263753731a21f7e0112b93d167c4ff346ef9497
theajaypee <110385542+theajaypee@users.noreply.github.com>
2022-08-25 10:43:26 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+
+var hamburger = document.querySelector('.hamburger-menu')
+var mobilelinks = document.querySelector('.mobile-link')
+var siderow = document.querySelector('.siderow')
+
+
+
+
+
+function Addclass() {
+    hamburger.classList.toggle('show')  
+    mobilelinks.classList.toggle('showlinks')
+    siderow.classList.toggle('showlinks')
+}
